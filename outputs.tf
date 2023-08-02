e62ad42973fdc72978b80ae9730b1cd07b19a32b
daniel khen <danielkhen20@gmail.com>
2023-08-02 18:25:37 +0300

    Auto-Commit: cr fixes

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "outgoing_name" {
-  description = "The name of the peering from the local to the remote virtual network."
-  value       = azurerm_virtual_network_peering.outgoing_peering.name
+  description = "The name of the peering from the first to the second virtual network."
+  value       = azurerm_virtual_network_peering.first_to_second.name
 }
 
 output "outgoing_id" {
-  description = "The id of the peering from the local to the remote virtual network."
-  value       = azurerm_virtual_network_peering.outgoing_peering.id
+  description = "The id of the peering from the first to the second virtual network."
+  value       = azurerm_virtual_network_peering.first_to_second.id
 }
 
 output "outgoing_object" {
-  description = "The peering from the local to the remote virtual network object."
-  value       = azurerm_virtual_network_peering.outgoing_peering
+  description = "The peering from the first to the second virtual network object."
+  value       = azurerm_virtual_network_peering.first_to_second
 }
 
 output "ingoing_name" {
-  description = "The name of the peering from the remote to the local virtual network."
-  value       = azurerm_virtual_network_peering.ingoing_peering.name
+  description = "The name of the peering from the second to the first virtual network."
+  value       = azurerm_virtual_network_peering.second_to_first.name
 }
 
 output "ingoing_id" {
-  description = "The id of the peering from the remote to the local virtual network."
-  value       = azurerm_virtual_network_peering.ingoing_peering.id
+  description = "The id of the peering from the second to the first virtual network."
+  value       = azurerm_virtual_network_peering.second_to_first.id
 }
 
 output "ingoing_object" {
-  description = "The peering from the remote to the local virtual network object."
-  value       = azurerm_virtual_network_peering.ingoing_peering
+  description = "The peering from the second to the first virtual network object."
+  value       = azurerm_virtual_network_peering.second_to_first
 }
